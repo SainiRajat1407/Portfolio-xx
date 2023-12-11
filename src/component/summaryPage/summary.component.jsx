545f3369a5d35ef9aa3704197546c089067fbdb5
SainiRajat1407 <rajrajat@sheridancollege.ca>
2023-12-11 17:31:16 -0500

    fixing the summary part

--- src/component/summaryPage/summary.component.jsx
+++ src/component/summaryPage/summary.component.jsx
@@ -1,17 +1,19 @@
 import "./summary.styles.scss";
-import React, {useState , useEffect} from "react";
+import React from "react";
 import backgroundVideo from '../../asset/videos/summary/earthtransparentvdo.mp4'
 import {motion} from 'framer-motion'
+import ScrollContext from "../../context/scroll/scrollContext";
 
 
 
 const tagline = [<>I <span className="don't-word">Don't</span> want a <span id="long-word">Long</span> life, I <span id="want-word">want</span> a <span id="big-word">big</span> one.</>]
 
-const Summary = () => {
-
+const Summary =  () => {
+  const scrollTracker = React.useContext(ScrollContext);
+  const scrollInVh = scrollTracker[2];
   return (
-    <div className="summary-container" id="Summary">
-      <div className="summary-tagline">
+    <div className="summary-container" id="Summary" style={{top : scrollInVh >= 0 && scrollInVh <= 99 ? "0":"", visibility : scrollInVh > 120 ? "hidden":""}}>
+      <div className="summary-tagline"  style={{top : scrollInVh >= 0 && scrollInVh <= 99 ? "17.5vh":""}}>
         {tagline}
       </div>
       <motion.video
